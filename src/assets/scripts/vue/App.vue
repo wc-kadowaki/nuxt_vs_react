1fcecb469ec8b24bdb528c1e3c38630a37f64f2d
WEBCREW_JPN¥hayato.kadowaki <hayato.kadowaki@webcrew.co.jp>
2022-07-04 16:53:08 +0900

    update: vue setup構文

--- src/assets/scripts/vue/App.vue
+++ src/assets/scripts/vue/App.vue
@@ -3,20 +3,22 @@
   <VueTest />
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup>
+import { onMounted } from "vue";
 import VueTest from "./components/VueTest";
 
-export default defineComponent({
-  name: 'App',
-  setup() {
-    return {
-      text: 'world'
-    }
-  },
-  components: {
-    VueTest,
-  }
+const text = 'world';
+
+const hoge = () => {
+  console.log('hoge');
+}
+
+onMounted(() => {
+  hoge();
 });
+
+const foo = async function () {
+  console.log('foo');
+}
 
 </script>
